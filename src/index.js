725aee4cdf148b777033493c7847932b78e66191
“ALEX <alexgtr84@gmail.com>
2019-03-08 11:00:20 +0100

    adds query to google data

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ router.get('/graphql', graphqlKoa({ schema }));
 // Setup the /graphiql route to show the GraphiQL UI
 router.get('/graphiql', graphiqlKoa({
   endpointURL: '/graphql',
+  
   // passHeader: `'Authorization': 'Bearer <test token>'`,
 }));
 
@@ -27,8 +28,46 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen(PORT, () => {
   const url = `http://localhost:${PORT}`;
-  console.log(`   GraphQL server started on:\n   ${url}\n\n`,
-    `➜ Open ${url}/graphiql to\n   start querying your API.\n\n`,
-    `➜ Point your GraphQL client apps to\n   ${url}/graphql\n`,
-  ' ---------------------------------------\n');
+  // console.log(`   GraphQL server started on:\n   ${url}\n\n`,
+  //   `➜ Open ${url}/graphiql to\n   start querying your API.\n\n`,
+  //   `➜ Point your GraphQL client apps to\n   ${url}/graphql\n`,
+  // ' ---------------------------------------\n');
 });
+
+// https://fj-servers.herokuapp.com/graphiql
+
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('postgres://tuqvrnyuewgtch:155699c54ff6dd27fdbdd24994c3c3f0fcc229fe28cb0da10cc69fde357edcb4@ec2-54-75-232-114.eu-west-1.compute.amazonaws.com:5432/d1rvf7fvtepa86?sslmode=require', {dialectOptions: {ssl: true}})
+//  sequelize
+//     .query('SELECT account_name FROM test2.ads_insights')
+//     .then(result => console.log(result))
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+// const AdInsights = sequelize.define('ads_insights', {
+//   add_id: Sequelize.STRING,
+//   account_name: Sequelize.STRING
+// })
+
+// AdInsights.find().then((result) => {
+//   console.log(result)
+// })
+
+
+// const getData = async () => {
+//   // Or you can simply use a connection uri
+//   const sequelize = await new Sequelize('postgres://tuqvrnyuewgtch:155699c54ff6dd27fdbdd24994c3c3f0fcc229fe28cb0da10cc69fde357edcb4@ec2-54-75-232-114.eu-west-1.compute.amazonaws.com:5432/d1rvf7fvtepa86', {ssl: true});
+//   sequelize.sync((data) => {
+//     console.log(data)
+//   }).then().catch((err) => console.log(err))
+//   // sequelize
+//   //   .query('SELECT * FROM test2.ads_insights')
+//   //   .then(result => console.log(result))
+// }
+
+// getData()
